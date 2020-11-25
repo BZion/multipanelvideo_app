759eeadb57b5a060e7fb2672e6e4705e89fc8140
BZion <alexander.bohm@outlook.com>
2020-11-25 11:48:17 +0100

    Begrüßungsvideo startet beim öffnen der App automatisch

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,142 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(VideoPlayerApp());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class VideoPlayerApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Video Player Demo',
+      home: VideoPlayerScreen('https://player.vimeo.com/external/475146519.sd.mp4?s=f727a2bf5f1586a0466d2c11ab6f65ee524c187a&profile_id=165'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class VideoPlayerScreen extends StatefulWidget {
+  String _link;
+  VideoPlayerScreen(_link) {
+    this._link=_link;
+  }
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState(_link);
+
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+  bool _isVisible = false;
+  String _link;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  _VideoPlayerScreenState(_link){
+    this._link=_link;
+  }
+
+  @override
+  void initState() {
+    // Create and store the VideoPlayerController. The VideoPlayerController
+    // offers several different constructors to play videos from assets, files,
+    // or the internet.
+    _controller = VideoPlayerController.network(_link,
+    )..initialize().then((_){setState(() {
+
+    });});
+    _controller.play();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Sie haben den Knopf so oft gedrückt:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: GestureDetector(onTap: onTap,onPanUpdate: onPanUpdate,
+          child:
+          Stack(children:[VideoPlayer(_controller, ),Center(
+              child:
+              ButtonTheme(
+                  height: 100.0,
+                  minWidth: 200.0,
+                  child: Visibility(
+                      maintainSize: true,
+                      maintainAnimation: true,
+                      maintainState: true,
+                      visible: _isVisible,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(60.0),
+                        color: Colors.transparent,
+                        textColor: Colors.white,
+                        onPressed: () {
+                        },
+                        child: Icon(
+                          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+                          size: 60.0,
+                        ),
+                      )))
+          ) ]),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  onTap(){
+    setState(() {
+      // If the video is playing, pause it.
+      if (_controller.value.isPlaying) {
+        _controller.pause();
+      } else {
+        // If the video is paused, play it.
+        _controller.play();
+      }
+    });
+  }
+  onPanUpdate(details) {
+    // swiping right
+    if (details.delta.dx > 0) {
+      print("swiping right");
+     /* _controller=VideoPlayerController.network('https://player.vimeo.com/external/475146519.sd.mp4?s=f727a2bf5f1586a0466d2c11ab6f65ee524c187a&profile_id=165',   )..initialize().then((_){setState(() {
+
+      });});*/
+      // swiping left
+    }else if (details.delta.dx < 0){
+      print("swiping left");
+    /*  _controller=VideoPlayerController.network('https://player.vimeo.com/external/475146619.sd.mp4?s=9e8126ba00891010a4f32c93b8ebf79f55d01c4e&profile_id=165',   )..initialize().then((_){setState(() {
+
+      });});*/
+    }
+  }
 }
+/*
+*
+* child: _controller.value.initialized
+                   ? AspectRatio(
+                 aspectRatio: _controller.value.aspectRatio,
+                      child:     Icon(Icons.play_arrow) ,
+
+*
+*     return GestureDetector(onTap: onTap,child: Container(
+
+             child: Icon(Icons.play_arrow) ,
+
+       )
+       );
+*
+* VideoPlayer(_controller),Positioned( // will be positioned in the top right of the container
+          top: 0,
+          right: 0,
+          child: Icon(
+          Icons.help,
+        ),
+        )])
+* */
