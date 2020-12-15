566190fed7524f50cc0615f9f5f5a1eec3407168
BZion <alexander.bohm@outlook.com>
2020-12-15 09:20:44 +0100

    assets Ordner für Zugriff auf externe Dateien hinzugefügt

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 void main() => runApp(VideoPlayerApp());
 
@@ -10,31 +11,97 @@ class VideoPlayerApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Video Player Demo',
-      home: VideoPlayerScreen('https://player.vimeo.com/external/475146519.sd.mp4?s=f727a2bf5f1586a0466d2c11ab6f65ee524c187a&profile_id=165'),
+      home:HomePage(),
     );
   }
+}
+
+class HomePage extends StatefulWidget{
+
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+
+  PageController _pageController = PageController(initialPage: 1);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> c = [
+      //Teaser
+      VideoPlayerScreen('https://player.vimeo.com/external/475146619.sd.mp4?s=9e8126ba00891010a4f32c93b8ebf79f55d01c4e&profile_id=165',_pageController),
+      //Begrüßung
+      VideoPlayerScreen('https://player.vimeo.com/external/475146519.sd.mp4?s=f727a2bf5f1586a0466d2c11ab6f65ee524c187a&profile_id=165',_pageController),
+      //Nach dem Teaser
+      VideoPlayerScreen('https://player.vimeo.com/external/475146749.sd.mp4?s=24bc3b95975e259841444e9b7f9a8e0c09badfb1&profile_id=165',_pageController),
+      //Platzhalter für die Login Seite
+      Container(child: Text("Login Seite")),
+      //Auswahlseite Anleitung
+      VideoPlayerScreen('https://player.vimeo.com/external/475147068.hd.mp4?s=3baa5f1602a825b64ca360228ca23e516b825ec6&profile_id=175', _pageController),
+      Auswahlseite()
+      //VideoPlayerScreen('https://player.vimeo.com/external/475146948.hd.mp4?s=02155c3f2f4bbaf859a774a6b307bf35492f8e27&profile_id=175', _pageController)
+    ];
+
+    return PageView(children: c, controller: _pageController);
+}
+}
+
+class Auswahlseite extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CarouselSlider(
+        options: CarouselOptions(height: 400.0),
+        items: [1,2,3,4,5].map((i) {
+          return Builder(
+            builder: (BuildContext context) {
+              return Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: BoxDecoration(
+                      color: Colors.amber
+                  ),
+                  child: Text('text $i', style: TextStyle(fontSize: 16.0),)
+              );
+            },
+          );
+        }).toList(),
+      )
+    );
+  }
+  
 }
 
 
 class VideoPlayerScreen extends StatefulWidget {
   String _link;
-  VideoPlayerScreen(_link) {
+  PageController _pageController;
+  VideoPlayerController _controller;
+  VideoPlayerScreen(_link, _pageController) {
+    print("VideoPlayerScreen");
     this._link=_link;
+    this._pageController = _pageController;
+    this._controller = VideoPlayerController.network(_link);
   }
 
   @override
-  _VideoPlayerScreenState createState() => _VideoPlayerScreenState(_link);
+  _VideoPlayerScreenState createState() => _VideoPlayerScreenState(_link,_pageController,_controller);
 
 }
 
 class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   VideoPlayerController _controller;
+  PageController _pageController;
   Future<void> _initializeVideoPlayerFuture;
   bool _isVisible = false;
   String _link;
+  Duration _duration = Duration(milliseconds: 200);
 
-  _VideoPlayerScreenState(_link){
+  _VideoPlayerScreenState(_link,_pageController,_controller){
     this._link=_link;
+    this._pageController = _pageController;
+  //  this._controller=_controller;
+    print("VideoPlayerScreenState");
   }
 
   @override
@@ -42,10 +109,14 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
     // Create and store the VideoPlayerController. The VideoPlayerController
     // offers several different constructors to play videos from assets, files,
     // or the internet.
-    _controller = VideoPlayerController.network(_link,
+    print("initState");
+    if(_controller==null){
+      print("_controller==null");
+      _controller = VideoPlayerController.network(_link,
     )..initialize().then((_){setState(() {
 
     });});
+    }
     _controller.play();
   }
 
@@ -53,42 +124,28 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
   void dispose() {
     // Ensure disposing of the VideoPlayerController to free up resources.
     _controller.dispose();
-
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      body: Center(
-        child: GestureDetector(onTap: onTap,onPanUpdate: onPanUpdate,
-          child:
-          Stack(children:[VideoPlayer(_controller, ),Center(
-              child:
-              ButtonTheme(
-                  height: 100.0,
-                  minWidth: 200.0,
-                  child: Visibility(
-                      maintainSize: true,
-                      maintainAnimation: true,
-                      maintainState: true,
-                      visible: _isVisible,
-                      child: FlatButton(
-                        padding: EdgeInsets.all(60.0),
-                        color: Colors.transparent,
-                        textColor: Colors.white,
-                        onPressed: () {
-                        },
-                        child: Icon(
-                          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-                          size: 60.0,
-                        ),
-                      )))
-          ) ]),
-        ),
-      ),
-    );
+    return GestureDetector(  onHorizontalDragEnd: (dragEndDetails) {
+      print("onHorizontalDragEnd");
+      print("_pageController.page"+_pageController.page.toString());
+      if (dragEndDetails.primaryVelocity < 0) {
+        // Page forwards
+        print('Move page forwards');
+        _pageController.nextPage(duration: _duration, curve: Curves.ease);
+        //_pageController.animateToPage(3, duration: _duration, curve: Curves.ease);
+      } else if (dragEndDetails.primaryVelocity > 0) {
+        // Page backwards
+        print('Move page backwards');
+        _pageController.previousPage(duration: _duration, curve: Curves.ease);
+      }
+    },onVerticalDragUpdate: (dragEndDetails){
+        print('Additional Content');
+    },onTap: onTap,
+    child:VideoPlayer(_controller));
   }
   onTap(){
     setState(() {
@@ -101,42 +158,5 @@ class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
       }
     });
   }
-  onPanUpdate(details) {
-    // swiping right
-    if (details.delta.dx > 0) {
-      print("swiping right");
-     /* _controller=VideoPlayerController.network('https://player.vimeo.com/external/475146519.sd.mp4?s=f727a2bf5f1586a0466d2c11ab6f65ee524c187a&profile_id=165',   )..initialize().then((_){setState(() {
 
-      });});*/
-      // swiping left
-    }else if (details.delta.dx < 0){
-      print("swiping left");
-    /*  _controller=VideoPlayerController.network('https://player.vimeo.com/external/475146619.sd.mp4?s=9e8126ba00891010a4f32c93b8ebf79f55d01c4e&profile_id=165',   )..initialize().then((_){setState(() {
-
-      });});*/
-    }
-  }
 }
-/*
-*
-* child: _controller.value.initialized
-                   ? AspectRatio(
-                 aspectRatio: _controller.value.aspectRatio,
-                      child:     Icon(Icons.play_arrow) ,
-
-*
-*     return GestureDetector(onTap: onTap,child: Container(
-
-             child: Icon(Icons.play_arrow) ,
-
-       )
-       );
-*
-* VideoPlayer(_controller),Positioned( // will be positioned in the top right of the container
-          top: 0,
-          right: 0,
-          child: Icon(
-          Icons.help,
-        ),
-        )])
-* */
